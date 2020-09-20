49b4fe9799d97c7d9c48425ed63a17879d79a90d
saikiranambati942 <saikiranambati942@gmail.com>
2020-09-21 00:10:09 +0200

    Added basic jwt based authentication functionality

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module BackendService
 go 1.15
 
 require github.com/gomodule/redigo v1.8.2
+
+require github.com/dgrijalva/jwt-go v3.1.0+incompatible
--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gomodule/redigo/redis"
 )
 
@@ -27,6 +29,7 @@ func main() {
 	//Conn := pool.Get()
 	//fmt.Println(Conn.Do("DEL", "user"))
 
+	http.HandleFunc("/client/welcome", Welcome)
 	http.HandleFunc("/client/register", Register)
 	http.HandleFunc("/client/login", Login)
 	if err := http.ListenAndServe("localhost:8081", nil); err != nil {
@@ -34,6 +37,8 @@ func main() {
 		os.Exit(1)
 	}
 }
+
+var jwtKey = []byte("my_secret_key")
 
 // Location struct defines the location of a player
 type Location struct {
@@ -45,6 +50,13 @@ type Location struct {
 type Player struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+// Claims struct will be encoded to a JWT.
+// We add jwt.StandardClaims as an embedded type, to provide fields like expiry time
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
 }
 
 //Register function is used to register the player
@@ -86,6 +98,59 @@ func Login(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &Claims{
+		Username: p.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "login sucessful")
+
+}
+
+// Welcome function
+func Welcome(w http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tokenString := cookie.Value
+	claims := &Claims{}
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	}
+	token, err := jwt.ParseWithClaims(tokenString, claims, keyFunc)
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !token.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
 }
